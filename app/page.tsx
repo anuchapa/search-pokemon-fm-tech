967d6e13b99cc22a2f30cf4ba9fbb8c10753e8a4
anuchapa <anuchapa.thongkam@gmail.com>
2026-02-08 13:02:59 +0700

    fix : add suspense for search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import { client} from "@/services/PokemonService";
+import { client } from "@/services/PokemonService";
 import { ApolloProvider } from "@apollo/client/react";
 import { PageDetail } from "./components/pageDetail";
+import { Suspense } from "react";
 
 
 
 export default function Page() {
   return (
     <ApolloProvider client={client}>
-      <PageDetail />
+      <Suspense fallback={<div>Loading Search...</div>}>
+        <PageDetail />
+      </Suspense>
     </ApolloProvider>
   )
 }
